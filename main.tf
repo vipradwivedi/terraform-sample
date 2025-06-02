4da80c782e3e9dbdff213efb0de005e5514a1fad
Vipra Dwivedi <vdwivedi@twilio.com>
2025-06-02 16:50:54 +0530

    add more resources

--- main.tf
+++ main.tf
@@ -1,5 +1,18 @@
-resource "random_string" "this" {
+resource "random_integer" "count" {
+  min = 1
+  max = 5
+  keepers = {
+    always_change = plantimestamp()
+  }
+}
+
+resource "random_string" "service" {
+  count = random_integer.count.result
+
   length  = var.length
   special = var.special
   lower   = var.lower
+  keepers = {
+    always_change = plantimestamp()
+  }
 }
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,3 @@
 output "random_string" {
-  value = "${var.service}-${var.env}-${random_string.this.result}"
+  value = "${var.service}-${var.env}-${random_string.this[0].result}"
 }
